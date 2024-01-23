b3ab444fd47f2bcdf3a0b91e625df9847e12d6a9
goriori <vladislav.borisov2010@gmail.com>
2024-01-24 00:48:01 +0300

    Update components

---
+++ src/components/modules/balance/popups/withdrawal/screens/sum-screen/SumScreen.events.js
@@ -0,0 +1,4 @@
+import { balance } from '@/components/modules/balance/popups/withdrawal/screens/sum-screen/SumScreen.options.js'
+export const onChangeSlotBalance = (slot) => {
+  balance.value = slot
+}
