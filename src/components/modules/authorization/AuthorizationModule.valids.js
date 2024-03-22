193db7b881fdcb1ae46b21f6877a215b891bb0a3
goriori <vladislav.borisov2010@gmail.com>
2024-03-22 13:38:41 +0300

    Update errors authorization module

--- src/components/modules/authorization/AuthorizationModule.valids.js
+++ src/components/modules/authorization/AuthorizationModule.valids.js
@@ -2,34 +2,35 @@ import {
   authMessages,
   formAuth,
   timeClosePopup,
- targetEntity
+  targetEntity,
 } from '@/components/modules/authorization/AuthorizationModule.options.js'
 import { useStateStore } from '@/store/stateStore.js'
 import { useRegular } from '@/utils/useRegular.js'
 import { ERRORS } from '@/configs/errors.js'
+
 export const onValidForm = async (targetEntity = 'email') => {
   const { emailTest, phoneTest } = useRegular()
-  if (targetEntity === 'email' && formAuth.value.username.trim().length === 0) throw ERRORS.ERROR_VALIDATION
-  else if (targetEntity.value === 'email' && !emailTest(formAuth.value.username)) throw ERRORS.ERROR_VALIDATION
-  else if (targetEntity.value === 'phone' && formAuth.value.username.trim().length === 0) throw ERRORS.ERROR_VALIDATION
-  else if (targetEntity.value === 'phone' && !phoneTest(formAuth.value.username)) throw ERRORS.ERROR_VALIDATION
-  else if (formAuth.value.password.trim().length === 0) throw ERRORS.ERROR_VALIDATION
+  if ( targetEntity === 'email' && formAuth.value.username.trim().length === 0) throw ERRORS.ERROR_VALIDATION.TYPE
+  else if ( targetEntity.value === 'email' && !emailTest(formAuth.value.username)) throw ERRORS.ERROR_VALIDATION.TYPE
+  else if ( targetEntity.value === 'phone' && formAuth.value.username.trim().length === 0) throw ERRORS.ERROR_VALIDATION.TYPE
+  else if ( targetEntity.value === 'phone' && !phoneTest(formAuth.value.username)) throw ERRORS.ERROR_VALIDATION.TYPE
+  else if (formAuth.value.password.trim().length === 0) throw ERRORS.ERROR_VALIDATION.TYPE
   return true
 }
 
 export const onErrorMessage = () => {
   const stateStore = useStateStore()
-  stateStore.globalPopupMessages.errorServer = true
+  stateStore.globalPopupMessages.error.show(ERRORS.ERROR_SERVER.MESSAGE)
   setTimeout(() => {
-    stateStore.globalPopupMessages.errorServer = false
+    stateStore.globalPopupMessages.error.close()
   }, timeClosePopup.value)
 }
 
 export const onErrorValid = () => {
   const stateStore = useStateStore()
-  stateStore.globalPopupMessages.errorValid = true
+  stateStore.globalPopupMessages.error.show(ERRORS.ERROR_VALIDATION.MESSAGE)
   setTimeout(() => {
-    stateStore.globalPopupMessages.errorValid = false
+    stateStore.globalPopupMessages.error.close()
   }, timeClosePopup.value)
 }
 export const onSuccessMessage = () => {
